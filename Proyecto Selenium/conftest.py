87ae925d99b780cd23a74af96dcaf62f81e0ffdf
Cristian Pimentel <cristianuni08@gmail.com>
2025-08-08 04:14:20 -0400

    .

---
+++ Proyecto Selenium/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    if rep.when == "call" and rep.failed:
+        driver = item.funcargs.get("driver")
+        if driver:
+            screenshot_path = f"screenshots/{item.name}.png"
+            driver.save_screenshot(screenshot_path)
